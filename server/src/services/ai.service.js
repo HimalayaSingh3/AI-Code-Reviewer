d9c18bf161d2a72badfd58b345542227d70415a0
Himalaya Singh <161754205+HimalayaSingh3@users.noreply.github.com>
2025-04-04 13:28:16 +0530

    Update ai.service.js

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("AIzaSyCqBSHKzglqMAVA58z9x8i96bYXEs6r7_8");
+const genAI = new GoogleGenerativeAI("");
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
   systemInstruction: `
